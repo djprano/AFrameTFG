d36fef864edbbde979400bb2bc50f0db086677a0
Víctor Jesús Temprano Hernández <djprano@gmail.com>
2022-12-08 13:03:38 +0100

    Problema de concurrencia en la lectura de ficheros

--- js/readApiLocalOpenSky.js
+++ js/readApiLocalOpenSky.js
@@ -5,20 +5,8 @@ class LocalApi {
         this.pathReference = './/openSkyData//';
         this.length = 333;
         this.openSkyApi = [];
-        this.index = 0;
         this.isLoaded = false;
-        for (let index = 0; index < this.length; index++) {
-
-            fetch(this.pathReference + this.fn + index + '.json')
-                .then((response) => response.json())
-                .then(json => {
-                    this.openSkyApi.push(json);
-                    if(this.openSkyApi.length == 333){
-                        this.isLoaded = true;
-                        this.openSkyApi.reverse();
-                    }
-                });
-        }
+        this.readJsonData(0);
     }
 
     getJsonOpenSky() {
@@ -27,9 +15,26 @@ class LocalApi {
         return this.openSkyApi.pop();
     }
 
+    //Función recursiva para cargar todos los ficheros de manera sincrona y secuencial
+    readJsonData(index){
+        fetch(this.pathReference + this.fn + index + '.json')
+        .then((response) => response.json())
+        .then(json => {
+            this.openSkyApi.push(json);
+            if (this.openSkyApi.length == 333) {
+                this.isLoaded = true;
+                this.openSkyApi.reverse();
+            }else{
+                this.readJsonData(++index);
+            }
+        });
+    }
+
+    
+
 }
 
-export{LocalApi}
+export { LocalApi }
 
 
 
