29289afca4ecf06f49b96ec37a92aefb501d1c05
Víctor Jesús Temprano Hernández <djprano@gmail.com>
2023-07-09 12:52:47 +0200

    suelo comprimido

--- js/configuration/madridconf.js
+++ js/configuration/madridconf.js
@@ -4,7 +4,7 @@ configuration.setMerConfig(40.0234170,40.7441446,-4.2041338,-3.2538165);
 configuration.setCamPosition(40.4893, -3.52254);
 configuration.setBuildingFileName('madrid_building');
 configuration.setFlightLocalFolder('_madrid');
-configuration.setMapRaster('Madrid_raster.jpg');
+configuration.setMapRaster('Madrid_raster_low.jpg');
 configuration.setMapDem('madrid_dem.bin');
 configuration.setLocalApiMode(true);
 configuration.setDaoInterval(2000);
