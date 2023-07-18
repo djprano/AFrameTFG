82fb03ffb26792bf574e71aa3a24035730b2b4cf
Víctor Jesús Temprano Hernández <djprano@gmail.com>
2023-07-18 09:57:36 +0200

    Madrid online

---
+++ js/configuration/madridconf_online.js
@@ -0,0 +1,13 @@
+import * as configuration from "./configurationModel.js";
+
+configuration.setMerConfig(40.0234170,40.7441446,-4.2041338,-3.2538165);
+configuration.setCamPosition(40.4893, -3.52254);
+configuration.setBuildingFileName('madrid_building');
+configuration.setFlightLocalFolder('_madrid');
+configuration.setMapRaster('Madrid_raster_low.jpg');
+configuration.setMapDem('madrid_dem.bin');
+configuration.setLocalApiMode(false);
+configuration.setDaoInterval(5100);
+configuration.setDaoLocalIndex(0);
+configuration.setApiUsuer('vjtemprano');
+configuration.setApiPassword('12345678');
